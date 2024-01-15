3db4cf7ab08d44c5b26c30b3801e8a49c3e5324f
Exp <17885980+exp111@users.noreply.github.com>
2024-01-15 17:09:01 +0100

    animal headers

--- gimp/export.py
+++ gimp/export.py
@@ -1,25 +1,45 @@
-import math
+import math, os
 image = gimp.image_list()[0]
-for i in range(2):
-    if i == 1:
-        pdb.gimp_image_scale(image, math.ceil(image.width / 2), math.ceil(image.height / 2))
-    # set everything as disabled
-    for layer in image.layers:
-        layer.visible = False
-    for group in image.layers:
-        if not group.layers:
+# set everything as disabled
+for layer in image.layers:
+    if pdb.gimp_item_get_color_tag(layer) == 8: # bg, always on
+        continue
+    layer.visible = False
+
+# export each group
+for group in image.layers:
+    if not hasattr(group, "layers"):
+        continue
+    group.visible = True
+    for layer in group.layers:
+        if pdb.gimp_item_get_color_tag(layer) == 8: # bg
             continue
-        group.visible = True
-        for layer in group.layers:
-            layer.visible = True
-            # set all other layers disabled
-            for l in group.layers:
-                if l == layer:
-                    continue
-                # grey color tag marks bg
-                if pdb.gimp_item_get_color_tag(l) == 8:
-                    continue
-                l.visible = False
-            # export
-            pdb.gimp_file_save(image, group, f"{group.name}-{layer.name}.jpg", "?")
-        group.visible = False
+        layer.visible = True
+        # set all other layers disabled
+        for l in group.layers:
+            if l == layer:
+                continue
+            # grey color tag marks bg, always on
+            if pdb.gimp_item_get_color_tag(l) == 8:
+                continue
+            l.visible = False
+        # export
+        base = group.name.lower() + "/" + group.name.lower() + "-" + layer.name.lower()
+        if not os.path.isdir(group.name.lower()):
+            os.mkdir(group.name.lower())
+        # duplicate and merge
+        dupe = pdb.gimp_image_duplicate(image)
+        content = pdb.gimp_image_merge_visible_layers(dupe, CLIP_TO_IMAGE)
+        pdb.gimp_file_save(dupe, content, base + ".jpg", "?")
+        # half scale
+        pdb.gimp_image_scale(dupe, math.ceil(dupe.width / 2), math.ceil(dupe.height / 2))
+        pdb.gimp_file_save(dupe, content, base + "-small.jpg", "?")
+        #delete dupe
+        pdb.gimp_image_delete(dupe)
+    group.visible = False
+
+# enable everything again
+for layer in image.layers:
+    layer.visible = True
+
+
